e57fa4359d6d63ead9b43f17c304c82693de418c
Smeet Hendre <smeethendre4@gmail.com>
2026-03-26 06:06:04 +0530

    feat: implement blog management features with CRUD operations and response handling

--- main-service/src/models/blogPost.model.js
+++ main-service/src/models/blogPost.model.js
@@ -1,18 +1,33 @@
-import { Schema } from "mongoose";
+import mongoose, { Schema } from 'mongoose'
 
 const blogPostSchema = new Schema(
   {
     blogTitle: {
-      type: String,
+      type:     String,
+      required: true,
+      trim:     true,
+    },
+    blogDescription: {
+      type:     String,
       required: true,
     },
-
-    blogDescription: {
-      type: String,
+    blogContent: {
+      type:     String,
       required: true,
+    },
+    coverImage: {
+      type:    String,    // URL string
+      default: null,
+    },
+    tags: [{
+      type: String,
+    }],
+    isPublished: {
+      type:    Boolean,
+      default: false,
     },
   },
-  { timestamps: true },
-);
+  { timestamps: true }
+)
 
-export { blogPostSchema };
+export const BlogPost = mongoose.model('BlogPost', blogPostSchema)
