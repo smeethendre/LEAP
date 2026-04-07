5416bb99487371575a3e69dc01c26fa5bd476535
Smeet Hendre <smeethendre4@gmail.com>
2026-04-07 15:59:49 +0530

    Remove main service files and dependencies for a complete refactor

--- main-service/src/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  const statusCode = err.statusCode || 500
-  res.status(statusCode).json({
-    success: false,
-    message: err.message || 'Internal server error',
-  })
-}
-
-export { errorHandler }
